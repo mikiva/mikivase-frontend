b8ae8c4fe1b59c5a0e3297e7d1bf9a1867e3ad37
Mikael Ivarsson <mikael.ivarsson4@gmail.com>
2022-11-17 17:46:43 +0100

    topnav different if scroll top

--- src/TopNav.js
+++ src/TopNav.js
@@ -4,11 +4,10 @@ import FadeInScrollTransition from "./FadeInScrollTransition";
 import TopNavLogo from "./TopNavLogo";
 import {useEffect, useState} from "react";
 import TopNavLinks from "./TopNavLinks";
-import {logDOM} from "@testing-library/react";
 
 function TopNav() {
     const TOP_OFFSET = 45;
-    const [show, setShow] = useState(true);
+    const [showTop, setShowTop] = useState(true);
     const [isTop, setIsTop] = useState(window.scrollY < TOP_OFFSET);
     const [prevPos, setPrevPos] = useState(0);
 
@@ -17,31 +16,33 @@ function TopNav() {
     function handleScroll() {
         const currentPos = window.scrollY;
         if (currentPos < prevPos) {
-            setShow(true)
-        } else setShow(false)
+            setShowTop(true)
+        } else setShowTop(false)
         setIsTop(currentPos < TOP_OFFSET);
-        setPrevPos(currentPos <= 0 ? 0 : currentPos);
+        const toPrevPos = currentPos <= 0 ? 0 : currentPos
+        setPrevPos(toPrevPos);
     }
 
     function handleHashChange() {
-        console.log("hashchange")
         setShowMenu(false);
 
     }
 
     useEffect(() => {
-        window.addEventListener("scroll", handleScroll, false);
-        window.addEventListener("hashChange", handleHashChange, false);
+        window.addEventListener("scroll", handleScroll);
+        window.addEventListener("hashChange", handleHashChange);
+
+        return () => {
+            window.removeEventListener("hashChange", handleHashChange);
+            window.removeEventListener("scroll", handleScroll);
+
+        }
     });
-
-    useEffect(() => {
-
-    }, [showMenu])
 
 
     return (
         <header
-            className={`${isTop ? "shadow-none top-2" : ""}  ${show ? "top-0 backdrop-blur bg-darkblue/75" : "-top-20"} bg-darkblue shadow-2xl transition-all flex box-border fixed left-0 h-20 w-full justify-between items-center z-10 px-6 md:px-16`}>
+            className={`${!showTop && !isTop ? "-top-20": ""}${showTop ^ isTop ? "top-0 backdrop-blur bg-darkblue/75" : ""} bg-darkblue transition-all flex box-border fixed left-0 h-20 w-full justify-between items-center z-10 px-6 md:px-16 ${isTop ? "shadow-none top-2" : "shadow-2xl "}  `}>
             <nav className="flex flex-1 justify-between items-center h-full">
                 <div>
                     <FadeInScrollTransition timeout={100} reversed>
@@ -56,12 +57,13 @@ function TopNav() {
                 <div className="hidden md:flex gap-3 pr-3">
                     <TopNavLinks/>
                 </div>
-                <div className="md:hidden">
+                <div className="md:hiddengi">
                     <button onClick={() => setShowMenu(true)}>menu</button>
 
                     <aside
                         className={`w-[min(60vw,350px)] transition-all flex flex-col gap-4 text-2xl justify-center items-center h-screen top-0 bottom-0 right-0 fixed bg-slate-900 shadow-xl ${showMenu ? "" : "right-[-100%]"} `}>
-                        <button onClick={() => setShowMenu(false)} className="text-7xl absolute top-4 right-10">&times;</button>
+                        <button onClick={() => setShowMenu(false)}
+                                className="text-7xl absolute top-4 right-10">&times;</button>
                         <TopNavLinks onClick={() => setShowMenu(false)}/>
                     </aside>
 
