0a3e537f03277a64a42ad94044baa60e813f6680
Mikael Ivarsson <mikael.ivarsson4@gmail.com>
2022-11-19 14:52:36 +0100

    update logo - again

--- src/TopNavLogo.js
+++ src/TopNavLogo.js
@@ -2,14 +2,12 @@ function TopNavLogo() {
 
     return (
         <>
-            <svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 187 187" className="fill-slate-300">
-                <g>
-                    <polygon points="113 113 113 187 76 187 113 113"/>
-                    <polygon points="0 113 0 187 37 187 0 113"/>
-                </g>
-                <polygon points="56.51 37 47.26 18.5 38.01 0 56.51 0 75 0 65.75 18.5 56.51 37"/>
-                <rect x="150" width="37" height="37" className="fill-accent"/>
-            </svg>
+<svg id="Layer_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 288 288" className="fill-slate-300">
+    <polygon points="144 144 144 288 72 288 144 144"/>
+    <polygon points="0 144 0 288 72 288 0 144"/>
+    <polygon points="72 72 72 72 36 0 108 0 72 72"/>
+    <rect x="216" width="72" height="72" className="fill-accent"/>
+</svg>
 
         </>
     )
@@ -18,3 +16,6 @@ function TopNavLogo() {
 }
 
 export default TopNavLogo;
+
+
+
